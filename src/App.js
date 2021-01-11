0f314ad8d466ab8c221360b5f0513ce40994cb7d
Coby Hong <cobyhong@gmail.com>
2021-01-11 10:46:24 -0800

    adding ability to remove items from table step.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import React, { Component } from 'react'
 import Table from './Table'
 
-
 class App extends Component {
-    render() {
-      const characters = [
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-      ]
-  
-      return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      )
-    }
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ],
   }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  render() {
+    const { characters } = this.state;
+    
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+      </div>
+    );
+  }
+
+}
 
 /*Note the last line in the App.js file. It makes the component available to be imported into other components or files (like we did in the index.js -- see the import).*/
 export default App
--- src/Table.js
+++ src/Table.js
@@ -49,8 +49,11 @@ const TableHeader = () => {
     const rows = props.characterData.map((row, index) => {
       return (
         <tr key={index}>
-          <td>{row.name}</td>
-          <td>{row.job}</td>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+        <button onClick={() => props.removeCharacter(index)}>Delete</button>
+        </td>
         </tr>
       )
     })
@@ -58,17 +61,15 @@ const TableHeader = () => {
     return <tbody>{rows}</tbody>
   }
 
-  class Table extends Component {
-    render() {
-      const { characterData } = this.props
+  const Table = props => {
+    const { characterData, removeCharacter } = props
   
-      return (
-        <table>
-          <TableHeader />
-          <TableBody characterData={characterData} />
-        </table>
-      )
-    }
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+      </table>
+    )
   }
   
 
